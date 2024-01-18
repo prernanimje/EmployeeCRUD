b196ecaa6c906064f66d0ce324580ab82e94482a
prernanimje <prernanimje5@gmail.com>
2024-01-18 13:02:45 +0530

    Employee Curd operation

---
+++ EmployeeCURD/src/main/java/com/io/Employee/EmployeeCURD/EmployeeCurdApplication.java
@@ -0,0 +1,18 @@
+package com.io.Employee.EmployeeCURD;
+
+import com.io.Employee.EmployeeCURD.controller.EmployeeController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class EmployeeCurdApplication {
+
+	public static void main(String[] args) {
+		var custom=SpringApplication.run(EmployeeCurdApplication.class, args);
+
+
+
+	}
+}
